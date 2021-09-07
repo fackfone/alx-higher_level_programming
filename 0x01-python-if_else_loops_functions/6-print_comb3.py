b7457624e7b2fc9af9b392c6d75d32a9487f94bc
fackfone <foneevanslindel36@gmail.com>
2021-09-07 18:10:07 +0100

    Fixes updates

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i != 99:
+    if( i // 10 == i % 10):
+        pass
+    if(
         print("{}{}".format(i // 10, i % 10), end=", ")
-print(i)
+    elif i == 99:
+        print(i)
