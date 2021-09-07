362761406627d238017a67e5ead82ced7cdcff50
fackfone <foneevanslindel36@gmail.com>
2021-09-07 20:49:04 +0100

    Fixes updates

--- 0x01-python-if_else_loops_functions/6-print_comb3.c
+++ 0x01-python-if_else_loops_functions/6-print_comb3.c
@@ -3,5 +3,5 @@ for i in range(10):
     for n in range(i, 10):
         if n != i and (i != 8 or n != 9):
             print("{}{}, ".format(i, n), end="")
-        elif i == 8 and n == 9:
+        if (i == 8 and n == 9):
             print("{}{}".format(i, n))
