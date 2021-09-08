b435776a0f7284a896a78ecc228f35fce88a4144
fackfone <foneevanslindel36@gmail.com>
2021-09-08 23:04:54 +0100

    Update the check cycle file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a single linked list has a cycle ijt
+ * @list: linked list to check
+ * Return - 0 if there is no cycle and 1 if there is one
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+	
+	if (list == NULL)
+		return(0);
+	current = list;
+	while (current->next)
+	{
+		if (current->next != list)
+		       current = current->next;
+		else if (current->next == NULL)
+			return(0);
+		else
+			return(1);
+	}
+	return(0);
+}
