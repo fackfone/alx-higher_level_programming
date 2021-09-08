97dd7b0db137e3386c5204b410f084c99f436dbd
fackfone <foneevanslindel36@gmail.com>
2021-09-08 23:45:33 +0100

    Update the check cycle file

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 {
 	listint_t *current;
 
-	if (list == NULL || list->next = NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
 	current = list;
 	while (current->next)
