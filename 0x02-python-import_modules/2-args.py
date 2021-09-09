41f030cf3919087a5593ed11ed1ae059e3f4f934
fackfone <foneevanslindel36@gmail.com>
2021-09-09 15:03:32 +0100

    Add the folder and all his filesclear

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ import sys
 length = len(sys.argv) - 1
 if (__name__ == "__main__"):
     print("{} {}{}".format(length,
-          "argument" if(length == 0 or length == 1) else "arguments",
+          "argument" if(length == 1) else "arguments",
           "." if (length == 0) else ":"))
     if length == 0:
-        sys.exit(0)
+        exit(0)
     for i in range(1, length + 1):
         print("{}: {}".format(i, sys.argv[i]))
