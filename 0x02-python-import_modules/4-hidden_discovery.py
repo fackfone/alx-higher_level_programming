96b9dc0a8a82b18d191da85dcc7deef87fc39c89
fackfone <foneevanslindel36@gmail.com>
2021-09-09 23:25:31 +0100

    Add folder and his files

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    dir()
+    for names in dir(hidden_4):
+        if names.startswith("__"):
+            continue
+        print("{:s}".format(names))
 
