3f5082715c2350392cc873d91d7de11d4aa39674
fackfone <foneevanslindel36@gmail.com>
2021-09-12 23:30:12 +0100

    Add the directory and the folders

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    if list == []:
-        return None
-    isDivisible = []
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            isDivisible.append(True)
-        else:
-            isDivisible.append(False)
-    return isDivisible
+a = 89
+b = 10
+c = a;a = b ;b = c   
+print("a={:d} - b={:d}".format(a, b))
