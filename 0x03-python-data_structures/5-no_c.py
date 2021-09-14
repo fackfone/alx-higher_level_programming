31255d471974f332dc94c72c2d062adfbd54fa08
fackfone <foneevanslindel36@gmail.com>
2021-09-14 22:37:33 +0100

    Add all the directory and the folders

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,14 +2,8 @@
 def no_c(my_string):
     if my_string is None:
         return
-    str_list = []
     string = ""
-    for char in my_string:
-        str_list.append(char)
-    if 'c' in str_list:
-        str_list.remove("c")
-    if 'C' in str_list:
-        str_list.remove("C")
-    for element in str_list:
-        string += element
+    for element in my_string:
+        if element != 'c' and element != 'C':
+            string += element
     return string
