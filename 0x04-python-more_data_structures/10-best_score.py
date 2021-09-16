3d354dd5b8ffaa1325e983f99ded6b421a52a527
fackfone <foneevanslindel36@gmail.com>
2021-09-16 02:54:31 +0100

    Add all the files and the directory

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max_score = 0
+    max_score = -1
+    favourite = None
     for key in a_dictionary:
         if max_score < a_dictionary[key]:
             max_score = a_dictionary[key]
