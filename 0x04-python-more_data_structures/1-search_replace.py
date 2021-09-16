861fabfdc7a3d4536a4907e0d82ecbc1b573b9a5
fackfone <foneevanslindel36@gmail.com>
2021-09-16 02:16:38 +0100

    Add all the files and the directory

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if my_list is None:
+        return None
     if search not in my_list:
         return my_list
     for integer in my_list:
