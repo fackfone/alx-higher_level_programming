f1760d53674c4953991af61296d476c37bc28145
fackfone <foneevanslindel36@gmail.com>
2021-09-27 22:58:51 +0100

    Add the files and contents

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            le,gth += 1
+            length += 1
         except (ValueError, TypeError):
             x += 1
     print("")
