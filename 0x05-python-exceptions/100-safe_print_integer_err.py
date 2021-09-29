5574882e581094f706dc98d9450070d3195b9bb5
fackfone <foneevanslindel36@gmail.com>
2021-09-29 22:10:32 +0100

    Add the files and the folder

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
-        print("{:d}".value)
+        print("{:d}".format(value))
     except Exception as x:
         print("Exception: {}".format(x), file=sys.stderr)
         return False
