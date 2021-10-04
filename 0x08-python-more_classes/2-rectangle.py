90a27bca9be2ab06f7cf018dd451a7b3453b1d34
fackfone <foneevanslindel36@gmail.com>
2021-10-04 22:59:18 +0100

    Add the files and the folder

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""
+'''
 This module defines a class
 Rectangle with width and height
 It also allows the caluclation
-of area and perimeter
-"""
+of area and perimeters
+'''
 
 
 class Rectangle:
-    """
+    '''
     Creates a class that defines a rectangle with
     defining the way to calculate area and perimeter
-    """
+    '''
 
     def __init__(self, width=0, height=0):
         self.__width = width
@@ -19,31 +19,27 @@ class Rectangle:
 
     @property
     def width(self):
-        self.__width = width
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
         if value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+            raise ValueError('width must be >= 0')
+        self.__width = value
 
     @property
     def height(self):
-        self.__height = height
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer")
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
         if value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+            raise ValueError('height must be >= 0')
+        self.__height = value
 
     def area(self):
         return self.__width * self.__height
