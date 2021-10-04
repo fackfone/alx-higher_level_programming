e279d59a737efd0d3f05ebce0041adef5df90628
fackfone <foneevanslindel36@gmail.com>
2021-10-04 23:14:14 +0100

    Add the files and the folder

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -20,7 +20,6 @@ class Rectangle:
     def __str__(self):
         s = ""
         if self.__width == 0 or self.__height == 0:
-            s = " "
             return s
         for i in range(self.__height):
             for j in range(self.__width):
