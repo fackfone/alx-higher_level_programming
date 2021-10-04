01c9ad9d696f8b6b7b2828e51964e95f8dc73e67
fackfone <foneevanslindel36@gmail.com>
2021-10-04 22:39:36 +0100

    Add the files and the folder

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle:
     def width(self, value):
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -40,7 +40,7 @@ class Rectangle:
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -32,18 +32,15 @@ class Rectangle:
 
     @property
     def width(self):
-        self.__width = width
         return self.__width
 
     @width.setter
     def width(self, value):
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
-
+            p²=p²p²ss²
     @property
     def height(self):
         self.__height = height
@@ -53,10 +50,9 @@ class Rectangle:
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
         return self.__width * self.__height
