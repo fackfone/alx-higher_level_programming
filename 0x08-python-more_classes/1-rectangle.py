5c3f385625f2b2a388cec73a8f570dc245476f27
fackfone <foneevanslindel36@gmail.com>
2021-10-04 22:24:20 +0100

    Add the files and the folder

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,8 +24,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
