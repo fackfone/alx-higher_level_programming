1858e88a7105e004634d57ab15bcf7ac19b3c50e
fackfone <foneevanslindel36@gmail.com>
2021-10-11 21:54:11 +0100

    Add the files and the folder

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
     """
     Return True is object is an instance of the specified class
     """
-    if type(obj) is a_class and issubclass(type(obj), a_class):
+    if type(obj) is not a_class and issubclass(type(obj), a_class):
         return True
     else:
         return False
