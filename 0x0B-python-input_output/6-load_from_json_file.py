26cd70057d43656a4c77a0a2dcc0450ecf61725a
fackfone <foneevanslindel36@gmail.com>
2021-10-12 22:14:09 +0100

    Add the files and the folder

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains a function
+that creates from a JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    This function creates an Object from a JSON file
+    """
+    with open(filename, 'r+') as file:
+        # decoded = json.JSONDecoder().decode(file.read()) also works
+        decoded = json.loads(file.read())
+        return decoded
